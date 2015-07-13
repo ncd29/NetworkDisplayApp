56c03a6125e7331bdff34f0a57bf45260d9c2a2f
ncd29 <ncd29@cornell.edu>
2015-07-13 12:43:41 -0400

    fixed many problems

--- app2.R
+++ app2.R
@@ -1,17 +1,14 @@
 #sociomatrixapp #3 - a network display app using igraph
-# need to add documentation, especially for the more involved output part
-# code seems very brute-force, room for improvement for sure
-# somewhat difficult to read
+
 library(shiny)
 library(igraph)
-# How do we get the first two columns of graph.data.frame to not be treated as
-# an "edge list"?
 
 # can use source instead to call Sociomatrix.R, but must save it in the app's folder
 wom <- read.csv("WOM3.csv", header = T, strip.white = T) # remove extra spaces in the middle with strip.white
 ns <- paste(as.character(wom$First.Name),as.character(wom$Last.Name))
 names <- list(ns)
 Collaborators <- as.character(wom$Collaborators)
+wom$School <- as.character(wom$School)
 
 # for all researchers, create a vector of all zeros, and change to a 1
 # if they have collaborated with a researcher in the corresponding column
@@ -91,7 +88,11 @@ d <- shinyServer(
     })
     
     output$text2 <- renderText({
-      paste(colnames(sociomatrix)[sociomatrix[input$selection,] == 1], ",")
+      sociodfReduced <- colnames(sociodf)[which(sociodf[input$selection,] == 1)]
+      if (identical(sociodfReduced,character(0)) | identical(sociodfReduced,"School")) {
+        paste(input$selection, " does not have any collaborators.")
+      }
+      else {paste(colnames(sociomatrix)[sociomatrix[input$selection,] == 1], ",")}
     })
     
     output$text1.2 <- renderText({
@@ -99,7 +100,11 @@ d <- shinyServer(
     })
     
     output$text2.2 <- renderText({
-      paste(colnames(sociomatrix)[sociomatrix[input$selection2,] == 1], ",")
+      sociodfReduced <- colnames(sociodf)[which(sociodf[input$selection2,] == 1)]
+      if (identical(sociodfReduced,character(0)) | identical(sociodfReduced,"School")){
+        paste(input$selection2, " does not have any collaborators.")
+      }
+      else {paste(colnames(sociomatrix)[sociomatrix[input$selection2,] == 1], ",")}
     })
     
     output$text1.3 <- renderText({
@@ -107,49 +112,36 @@ d <- shinyServer(
     })
     
     output$text2.3 <- renderText({
-      paste(colnames(sociomatrix)[sociomatrix[input$selection3,] == 1], ",")
+      sociodfReduced <- colnames(sociodf)[which(sociodf[input$selection3,] == 1)]
+      if (identical(sociodfReduced,character(0)) | identical(sociodfReduced,"School")) {
+        paste(input$selection3, " does not have any collaborators.")
+      }
+      else {paste(colnames(sociomatrix)[sociomatrix[input$selection3,] == 1], ",")}
     })
     
     output$network <- renderPlot({
       sociodfFirst2 <- sociodf[input$selection2,1]
       sociodfReduced2 <- colnames(sociodf)[which(sociodf[input$selection2,] == 1)]
       combined2 <- (merge(sociodfFirst2,sociodfReduced2))
-      #row.names(combined2) <- 1:length(row.names(combined2))
       combined2[,1] <- as.character(combined2[,1])
       combined2[,2] <- as.character(combined2[,2])
-      print(combined2)
       sociodfFirst3 <- sociodf[input$selection3,1]
       sociodfReduced3 <- colnames(sociodf)[which(sociodf[input$selection3,] == 1)]
       combined3 <- (merge(sociodfFirst3,sociodfReduced3))
-      #row.names(combined3) <- 1:length(row.names(combined3))
       combined3[,1] <- as.character(combined3[,1])
       combined3[,2] <- as.character(combined3[,2])
-      print(combined3)
       sociodfFirst <- sociodf[input$selection,1]
       sociodfReduced <- colnames(sociodf)[which(sociodf[input$selection,] == 1)]
-      print("1")
       combined <- (merge(sociodfFirst,sociodfReduced))
-      print(combined)
       combined <- unique(combined)
-      print("#1")
-      print("2")
-      #row.names(combined) <- 1:length(row.names(combined))
       combined[,1] <- as.character(combined[,1])
       combined[,2] <- as.character(combined[,2])
-      # can't just be 5-8 needs to be the length of combined2 and combined3
-      print("3")
-      combined[(length(rownames(unique(combined)))+1):(length(rownames(unique(combined2)))+length(rownames(unique(combined)))),] <- unique(combined2)
-      print(combined)
-      print("#2")
-      combined[(length(rownames(unique(combined)))+1):(length(rownames(unique(combined3)))+length(rownames(unique(combined)))),] <- unique(combined3)
-      print("4")
-      print(combined)
-      print("#3")
-      print("5")
-      sociogdf <- graph.data.frame(unique(combined))  #turns the df into a data frame with edges that can be read by graph.data.frame
-      # unique is important,it prevents errors and makes sure the arrows are only drawn once when one professor is selected in all three inputs
-      print("6")
-      plot(sociogdf)
+      combined.all <- rbind(combined, combined2, combined3)
+      if (is.null(rownames(combined.all))) {}
+      else{
+        sociogdf <- graph.data.frame(unique(combined.all))  #turns the df into a data frame with edges that can be read by graph.data.frame
+        plot(sociogdf)
+      }
     })
   })
 shinyApp(ui = n, server = d)
